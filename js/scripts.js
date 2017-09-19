030c384452a5a5a4a9f29a9fc059e8a52b934173
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-09-19 21:23:41 +0200

    Zadanie 11.5

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// scripts.js
+// Excercise 11.5
+
+function Button(text){
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+	var self = this;
+	this.$element = $('<button>');
+	this.$element.text(this.text);
+	this.$element.click(function() {
+		alert(self.text);
+	});
+	$('body').append(this.$element);
+	}
+};
+
+var btn1 = new Button('Hello Marta!');
+
+btn1.create();
